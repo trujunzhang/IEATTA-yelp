37e3a5a8fb72ae42b59c53aa1dd67fd3d45e76ac
trujunzhang <trujunzhang@gmail.com>
2017-07-05 12:24:35 +0800

    Ieatta.

--- back4app/ieatta.py
+++ back4app/ieatta.py
@@ -161,7 +161,7 @@ class IEATTADemo(object):
             images = restaurant['images']
             _import = RestaurantImporter(restaurant, self.users, self.recipes)
             _import.save_restaurant()
-            # _import.save_photos_for_restaurant(images)
+            _import.save_photos_for_restaurant(images)
             # _import.save_event()
 
 
