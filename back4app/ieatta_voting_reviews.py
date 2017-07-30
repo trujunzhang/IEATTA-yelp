b75811c8830adfa0e0ea12107e39dc2547b946dd
trujunzhang <trujunzhang@gmail.com>
2017-07-30 13:37:32 +0800

    Ieatta.

---
+++ back4app/ieatta_voting_reviews.py
@@ -0,0 +1,54 @@
+import json
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+from yelp.parse.parse_utils import ParseUserUtils, ParseRestaurantUtils, ParseEventUtils, ParseRecipeUtils, \
+    ParsePhotoUtils, ParseReviewUtils, get_object_by_type, Review
+
+
+class IEATTAVotingReviews(object):
+    def __init__(self):
+        super(IEATTAVotingReviews, self).__init__()
+
+        with open('parse_yelp_voting_reviews.json') as data_file:
+            self.data = json.load(data_file)
+
+    def __get_voting_reviews(self, review_voter):
+        _voting = review_voter['voting']
+
+        voting_dict = {}
+        for r_index, review_type in enumerate(_voting.keys()):
+            review_ids = _voting[review_type]
+
+            voted_reviews = []
+            for v_index, review_id in enumerate(review_ids):
+                _p_review = get_object_by_type(Review.Query, {'testId': review_id})
+                voted_reviews.append(_p_review)
+
+            voting_dict[review_type] = voted_reviews
+            pass
+
+    def import_all_base_array(self):
+        for r_index, review_voter in enumerate(self.data['userVoting']):
+            logging.info("  *** step {} ".format(r_index + 1))
+            _p_user = ParseUserUtils.get_user(review_voter, 'userTestId')
+
+            self.__get_voting_reviews(review_voter)
+            pass
+
+
+def main():
+    logging.info("  Start voting IEATTA reviews! ")
+    utils = IEATTAVotingReviews()
+
+    logging.info("     ")
+    logging.info("  * {} ".format('Ready'))
+
+    utils.import_all_base_array()
+
+
+if __name__ == '__main__':
+    main()
