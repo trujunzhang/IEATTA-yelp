6e0dd2596a2b6813428c450e36f286b8f5bcdee4
trujunzhang <trujunzhang@gmail.com>
2017-08-07 18:17:51 +0800

    Ieatta.

--- back4app/google/location_to_address.py
+++ back4app/google/location_to_address.py
@@ -2,12 +2,26 @@ import json
 
 import logging
 
+import googlemaps
+from datetime import datetime
+
 
 class LocationToAddress(object):
     def __init__(self):
         super(LocationToAddress, self).__init__()
 
+        self.gmaps = googlemaps.Client(key='AIzaSyBKOlhF3Qw15YJgnCiyyL1wYI3VOXAeTQU')
+
+    def to_location(self):
+        # Geocoding an address
+        geocode_result = self.gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+        pass
+
     def to_address(self):
+        # Look up an address with reverse geocoding
+        reverse_geocode_result = self.gmaps.reverse_geocode((40.714224, -73.961452))
+
         pass
 
 
@@ -18,6 +32,7 @@ def main():
     logging.info("     ")
     logging.info("  * {} ".format('Ready'))
 
+    # utils.to_location()
     utils.to_address()
 
 
