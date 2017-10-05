029d550a519ffbd0ce2e495d938f11eb9abf17c4
trujunzhang <trujunzhang@gmail.com>
2017-10-06 07:48:44 +0800

    Ieatta.

--- back4app/ieatta_relations.py
+++ back4app/ieatta_relations.py
@@ -6,7 +6,8 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 from yelp.parse.parse_utils import ParseUserUtils, ParseRestaurantUtils, ParseEventUtils, ParseRecipeUtils, \
-    ParsePhotoUtils, get_object_by_type, Restaurant, Event, Recipe, ParsePeopleInEventUtils, ParseRelationUtil
+    ParsePhotoUtils, get_object_by_type, Restaurant, Event, Recipe, ParsePeopleInEventUtils, ParseRelationUtil, \
+    get_table_list
 
 
 class IEATTARelation(object):
@@ -16,7 +17,20 @@ class IEATTARelation(object):
         with open('parse_yelp_relations.json') as data_file:
             self.data = json.load(data_file)
 
-    def __get_recipes(self, recipeIds):
+        self.recipes_dict = self.__get_recipes_dict()
+
+        pass
+
+    def __get_recipes_dict(self):
+        list = get_table_list('recipe')
+        recipes_dict = {}
+
+        for recipe in list:
+            recipes_dict[recipe.testId] = recipe
+
+        return recipes_dict
+
+    def __get_recipes_pointer_array(self, recipeIds):
         p_recipes = []
         for recipeId in recipeIds:
             p_recipes.append(get_object_by_type(Recipe.Query, {'testId': recipeId}))
@@ -43,14 +57,14 @@ class IEATTARelation(object):
                 for p_index, people_in_event in enumerate(event['peopleInEvent']):
                     _p_user = ParseUserUtils.get_user(people_in_event, 'userTestId')
                     # then save it.
-                    ParsePeopleInEventUtils.save_people_in_event(point_restaurant, point_event,
-                                                                 _p_user, people_in_event)
+                    point_people_in_event_ = ParsePeopleInEventUtils.save_people_in_event(point_restaurant, point_event,
+                                                                                          _p_user, people_in_event)
                     logging.info(
                         "     save relation(PeopleInEvent) between restaurant, event and user, {} ".format(p_index + 1))
                     logging.info("     ")
 
                     for recipe_index, recipe_test_id in enumerate(people_in_event['recipeIds']):
-                        ParseRecipeUtils.relate_recipe(recipe_test_id, point_restaurant, point_event, _p_user)
+                        # ParseRecipeUtils.relate_recipe(recipe_test_id, point_restaurant, point_event, _p_user)
                         logging.info("     save relation(Recipe), {} ".format(recipe_index + 1))
 
 
